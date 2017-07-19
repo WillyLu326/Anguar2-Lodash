e1e4448630ae6cd46f32fa6799e0912fd51bf701
WillyLu326 <luzh0326@gmail.com>
2017-07-18 20:34:52 -0400

    implement lodash concat

--- src/app/concat/concat.component.ts
+++ src/app/concat/concat.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as _ from 'lodash';
+
 @Component({
   selector: 'app-concat',
   templateUrl: './concat.component.html',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConcatComponent implements OnInit {
 
+  array1: any[];
+  array2: any[];
+  concatedArray: any[];
+
   constructor() { }
 
   ngOnInit() {
+    this.array1 = [1, 2, 3];
+    this.array2 = [4, 5, 6];
+    this.concatedArray = _.concat(this.array1, this.array2);
   }
 
 }
