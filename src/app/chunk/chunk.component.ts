b74b6113c08baf0761c417e13bdb83025b02b8de
WillyLu326 <luzh0326@gmail.com>
2017-07-18 20:24:57 -0400

    implement lodash chunk

--- src/app/chunk/chunk.component.ts
+++ src/app/chunk/chunk.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as _ from 'lodash';
+
 @Component({
   selector: 'app-chunk',
   templateUrl: './chunk.component.html',
@@ -10,6 +12,9 @@ export class ChunkComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let chunkedArray = _.chunk(array, 4);
+    console.log(chunkedArray);
   }
 
 }
